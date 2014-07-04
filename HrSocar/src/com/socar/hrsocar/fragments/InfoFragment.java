83bf539901271188b011817b781c538c5403521d
ulviibrahimov <ulvi@ualberta.ca>
2014-07-04 16:18:20 +0500

    layout header

---
+++ HrSocar/src/com/socar/hrsocar/fragments/InfoFragment.java
@@ -0,0 +1,17 @@
+package com.socar.hrsocar.fragments;
+import com.socar.hrsocar.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InfoFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+}
