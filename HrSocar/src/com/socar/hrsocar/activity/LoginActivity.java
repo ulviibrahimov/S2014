56801da4cfdb4bd264164bd89d37b216fd2e30b9
ulviibrahimov <ulvi@ualberta.ca>
2014-07-04 10:51:45 +0500

    Project setup

---
+++ HrSocar/src/com/socar/hrsocar/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.socar.hrsocar.activity;
+
+import com.socar.hrsocar.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+}
